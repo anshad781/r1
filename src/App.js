2807859eb507c43638d9fa8143b2aa56765022e6
anshad781 <aaanshad09@gmail.com>
2024-02-08 21:57:53 +0530

    nana

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="rose">
+        <div className="petal" />
+        <div className="petal" />
+        <div className="petal" />
+        <div className="petal" />
+       
+      </div>
+      <h1 className='title'>so imagine this as a rose</h1>
+      <b className='title2'>and what are we supposed to do on propose day</b>
+      <b className='title3'>so will you be my valentine??</b>
     </div>
   );
 }
